7a3054c83e4177d8113db77ab9c991d3adc8bfff
Elman Au <elman.au@gmail.com>
2018-11-06 21:46:33 -0700

    Editted options menu

--- memberOptions.py
+++ memberOptions.py
@@ -1,4 +1,4 @@
-#import the 5 program functions
+# import the 5 program functions
 from PostRideRequest import*
 from RideRequestOptions import*
 from SearchForRide import*
@@ -6,39 +6,42 @@ from BookingOptions import*
 from OfferRide import*
 
 def memberOptions(user):
-    print("========")
-    print("What would you like to do?")
-    print("========")
-    print("A - Offer a ride")
-    print("B - Search for a ride")
-    print("C - Book a member")
-    print("D - Post a request")
-    print("E - Search and delete ride requests")
-    print("Z - Logout")
-    print("========")
-    print("Type A B C D or E and hit enter to select option")
-
     # Handles user menu choice
     while True:
+
+        print("========")
+        print("Main menu: What would you like to do?")
+        print("========")
+        print("A - Offer a ride")
+        print("B - Search for a ride")
+        print("C - Book a member or cancel booking")
+        print("D - Post a request")
+        print("E - Search or delete ride requests")
+        print("Z - Logout")
+        print("========")
+        print("Type A B C D E or Z and hit enter to select option.")
+
         userInput = input("Function> ")
         if userInput.upper() in ('A', 'B', 'C', 'D', 'E', 'Z'):
-            break
+            if userInput.upper() == 'A':
+                OfferRide(user)
+                print("========")
+                print("Ride successfully created")
+            elif userInput.upper() == 'B':
+                RideSearch(user)
+                print("========")
+                print("Message sent to driver")
+            elif userInput.upper() == 'C':
+                BookingOptions(user)
+            elif userInput.upper() == 'D':
+                PostRequest(user)
+            elif userInput.upper() == 'E':
+                RequestOptions(user)
+            elif userInput.upper() == 'Z':
+                print("========")
+                print("You are now logged out.")
+                break
         else:
             print("Input value not valid, please retry your option.")
             continue
-
-    if userInput.upper() == 'A':
-        OfferRide(user)
-    elif userInput.upper() == 'B':
-        RideSearch(user)
-    elif userInput.upper() == 'C':
-        BookMember(user)
-    elif userInput.upper() == 'D':
-        PostRequest(user)
-    elif userInput.upper() == 'E':
-        RequestOptions(user)
-    elif userInput.upper() == 'Z':
-        print("========")
-        print("You are now logged out.")
-        return
-    return True
+    return
